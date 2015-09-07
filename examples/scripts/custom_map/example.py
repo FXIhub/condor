595b007ac6e7ac185bf777dc79903d8337ccc120
Max Hantke <maxhantke@gmail.com>
2015-09-07 12:19:04 +0200

    added missing example file

---
+++ examples/scripts/custom_map/example.py
@@ -0,0 +1,46 @@
+import numpy
+import matplotlib.pyplot as pypl
+import os
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+import condor
+
+import logging
+logger = logging.getLogger("condor")
+#logger.setLevel("DEBUG")
+#logger.setLevel("WARNING")
+logger.setLevel("INFO")
+
+if False:
+    N = 1
+    extrinsic_quaternion = numpy.array([ 0.05419534,  0.29519801, -0.91633864,  0.26503677])
+    rotation_formalism= "quaternion"
+    rotation_values = [extrinsic_quaternion]
+else:
+    N = 2
+    rotation_formalism="random"
+    rotation_values = None
+
+# Source
+src = condor.Source(wavelength=1.0E-9, pulse_energy=1E-3, focus_diameter=1E-6)
+# Sample
+sam = condor.Sample()
+# Detector
+det = condor.Detector(distance=1.0, pixel_size=300E-6, nx=256, ny=256)
+# Map
+print "Simulating map"
+par = condor.ParticleMap(diameter=600E-9, material_type="cell", geometry="custom",
+                         map3d_filename="../../map3d.h5", map3d_dataset="data", dx=5E-9,
+                         rotation_formalism=rotation_formalism, rotation_values=rotation_values)
+s = "custom_map"
+sam.append_particle(par, s)
+E = condor.Experiment(src, sam, det)
+
+W = condor.utils.log.CXIWriter("./condor.cxi")
+for i in range(N):
+    res = E.propagate()
+    real_space = numpy.fft.fftshift(numpy.fft.ifftn(res["entry_1"]["data_1"]["data_fourier"]))
+    pypl.imsave(this_dir + "/simple_test_%s_%i.png" % (s,i), numpy.log10(res["entry_1"]["data_1"]["data"]))
+    pypl.imsave(this_dir + "/simple_test_%s_%i_rs.png" % (s,i), abs(real_space))
+    W.write(res)
+W.close()
