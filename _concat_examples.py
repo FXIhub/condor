89f206d7dabaffc1fce1f2c64e7932a1bf9eee20
Max Felix Hantke <max.hantke@refeyn.com>
2020-06-27 16:25:47 +0100

    Towards Windows support

--- _concat_examples.py
+++ _concat_examples.py
@@ -21,18 +21,18 @@ if __name__ == "__main__":
 
     print("Concatenating example configuration files...")
 
-    src = here + "/examples/configfile/source.conf"
-    det = here + "/examples/configfile/detector.conf"
+    src = os.path.join(here, 'examples', 'configfile', 'source.conf')
+    det = os.path.join(here, 'examples', 'configfile', 'detector.conf')
 
     for m in ["particle_sphere","particle_spheroid","particle_map","particle_atoms"]:
-        par = here + ("/examples/configfile/%s.conf" % m)
+        par = os.path.join(here, 'examples', 'configfile', '%s.conf' % m)
         infilenames = [src, par, det]
-        d = here + ("/examples/configfile/%s" % m)
+        d = os.path.join(here, 'examples', 'configfile', '%s' % m)
         if not os.path.exists(d):
             os.mkdir(d)
             # Ensure that user can write to the directory if setup is run by root
             os.system("chmod a+rwx %s" % d)
-        outfilename = d + "/condor.conf"
+        outfilename = os.path.join(d, "condor.conf")
         print("Concatenating " + outfilename + "...")
         concatenate_files(infilenames, outfilename, 2)
     
